3182e186ceea30800be6788cf3f206c2c2686452
yogi4298 <46456366+yogi4298@users.noreply.github.com>
2019-01-08 02:42:33 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <td widht="298" heoght="300" aligen="center">
 <p><strong>Nama  :Yogi setiawan</strong></p>
-<p><strong>Nim   :215280062</strong></p>
+<p><strong>Nim   :215280069</strong></p>
 <p><strong>Phone :082879654400</strong></p>
 <p><strong>Fax   :syogi4298@gmail.com</strong></p>
 <p><strong>MAIWA</strong></p></td>
