b690813bdf02e8669218fd08f29bad860f53e8d2
yogi4298 <46456366+yogi4298@users.noreply.github.com>
2019-01-08 02:32:06 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,9 @@
+<table widht="337">
+
+<td widht="298" heoght="300" aligen="center">
+<p><strong>Nama  :Yogi setiawan</strong></p>
+<p><strong>Nim   :215280062</strong></p>
+<p><strong>Phone :082879654400</strong></p>
+<p><strong>Fax   :syogi4298@gmail.com</strong></p>
+<p><strong>MAIWA</strong></p></td>
+</table>
